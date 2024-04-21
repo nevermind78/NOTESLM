28cfe6611542c252573dbd9c25cc4b1b154368b6
NEVERMIND78 <abdallah.khemais@gmail.com>
2024-04-21 18:24:26 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pandas as pd
+
+# Chargement du fichier CSV
+df = pd.read_csv("1LM.csv", delimiter=";")
+
+# Titre de l'application
+st.title("NOTES DE DS PYTHON 1LM A.U 2023-2024")
+
+# Champ de saisie pour l'email de l'étudiant
+email = st.text_input("Saisissez votre email")
+
+# Vérification si l'email existe dans le fichier CSV
+if email:
+    if email in df["Email"].values:
+        # Récupération des informations de l'étudiant correspondant à l'email
+        etudiant = df[df["Email"] == email]
+        nom = etudiant["Name"].values[0]
+        groupe = etudiant["GROUP"].values[0]
+        note = etudiant["Note"].values[0]
+        
+        # Affichage des informations de l'étudiant
+        st.success(f"Nom de l'étudiant : {nom}")
+        st.success(f"Groupe de l'étudiant : {groupe}")
+        st.success(f"La note de l'étudiant est : {note}")
+    else:
+        st.error("Email non trouvé")
